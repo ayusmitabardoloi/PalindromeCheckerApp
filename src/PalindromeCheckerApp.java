b30e11c66161ea8b2d679b33684b6183a354d86c
ayusmitabardoloi48@gmail.com <ayusmitabardoloi48@gmail.com>
2026-02-23 15:46:50 +0530

    UC2: Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,10 +1,15 @@
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome Checker App");
-    }
-}
 
-        scanner.close();
+        System.out.println("Welcome to Palindrome Checker App");
+
+        String word = "madam";
+
+        if(word.equals(new StringBuilder(word).reverse().toString())) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is NOT a Palindrome");
+        }
     }
 }
