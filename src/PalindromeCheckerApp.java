bf7a6c34afa2788d16114f82d73fdde35cb5cc34
ayusmitabardoloi48@gmail.com <ayusmitabardoloi48@gmail.com>
2026-02-23 15:35:23 +0530

    hello

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,29 @@
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
+
+    // Method to check palindrome
+    public static boolean isPalindrome(String input) {
+        String cleaned = input.replaceAll("\\s+", "").toLowerCase();
+        String reversed = new StringBuilder(cleaned).reverse().toString();
+        return cleaned.equals(reversed);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("===== Welcome to Palindrome Checker App =====");
+
+        System.out.print("Enter a word or number: ");
+        String input = scanner.nextLine();
+
+        if (isPalindrome(input)) {
+            System.out.println(input + " is a Palindrome ✅");
+        } else {
+            System.out.println(input + " is NOT a Palindrome ❌");
+        }
+
+        scanner.close();
+    }
 }
